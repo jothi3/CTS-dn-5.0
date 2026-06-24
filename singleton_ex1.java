242ee24b8b078ea4503dc980c279fca6659a89b7
jothishree <jothielan005@gmail.com>
2026-06-24 17:56:28 +0530

    Refactor Logger class for singleton pattern

--- singleton_ex1.java
+++ singleton_ex1.java
@@ -1,14 +1,14 @@
 public class Logger {
 
-    // Private static instance of the class
+  
     private static Logger instance;
 
-    // Private constructor prevents instantiation from other classes
+  
     private Logger() {
         System.out.println("Logger instance created.");
     }
 
-    // Public method to provide access to the single instance
+  
     public static Logger getInstance() {
         if (instance == null) {
             instance = new Logger();
@@ -16,7 +16,7 @@ public class Logger {
         return instance;
     }
 
-    // Sample logging method
+    
     public void log(String message) {
         System.out.println("LOG: " + message);
     }
