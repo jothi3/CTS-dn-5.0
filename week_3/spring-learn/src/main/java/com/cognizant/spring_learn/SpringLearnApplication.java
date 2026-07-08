62769ed1e77c4eb048fedc50f44790b8bb60f3e3
jothi3 <jothielan005@gmail.com>
2026-07-08 18:53:57 +0530

    Added Week 3 Exercise 2 - Spring Core Countries

--- week_3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ week_3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,13 +1,42 @@
 package com.cognizant.spring_learn;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringLearnApplication.class, args);
+
+		displayCountries();
+
+	}
+
+	public static void displayCountries() {
+
+		System.out.println("START");
+
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("country.xml");
+
+		List<Country> countries =
+				(List<Country>) context.getBean("countryList");
+
+		for (Country country : countries) {
+
+			System.out.println(country);
+
+		}
+
+		context.close();
+
+		System.out.println("END");
+
 	}
 
 }
