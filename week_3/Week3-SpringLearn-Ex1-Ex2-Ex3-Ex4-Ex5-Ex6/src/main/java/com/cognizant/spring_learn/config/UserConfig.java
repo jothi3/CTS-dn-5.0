ccafc45d78a5297bc61a6a73be04a92157b60aa5
jothi3 <jothielan005@gmail.com>
2026-07-10 12:53:27 +0530

    Week 3 Ex6 JWT Authentication

---
+++ week_3/Week3-SpringLearn-Ex1-Ex2-Ex3-Ex4-Ex5-Ex6/src/main/java/com/cognizant/spring_learn/config/UserConfig.java
@@ -0,0 +1,21 @@
+package com.cognizant.springlearn.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class UserConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user")
+                        .password("{noop}pwd")
+                        .roles("USER")
+                        .build()
+        );
+    }
+}
---
+++ week_3/Week3-SpringLearn-Ex1-Ex2-Ex3-Ex4-Ex5-Ex6/src/main/java/com/cognizant/spring_learn/util/JwtUtil.java
@@ -0,0 +1,21 @@
+package com.cognizant.spring_learn.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final String SECRET = "secretkey";
+
+    public static String generateToken(String username) {
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1200000))
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+    }
+}
